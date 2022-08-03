b7b82e9d77623e1ce10c736694c050bdf645d307
Hyunjoo Lee <hyunjoo0572@gmail.com>
2022-08-03 23:13:27 +0900

    Modify: review 반영 mixin사용 , conflict 해결

--- src/styles/common.js
+++ src/styles/common.js
@@ -1,4 +1,8 @@
 export const setBorder = (border, radius = '15px') => `
   border: ${border};
   border-radius: ${radius};
+  `;
+export const setFont = (size = '30px', weight = '700') => `
+font-size: ${size};
+font-weight: ${weight};
 `;
